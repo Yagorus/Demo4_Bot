270f743107bfcaa5e89a0a42202cdb99bf586021
Yagorus <yegor.sidenko@gmail.com>
2022-01-24 01:54:55 +0200

    add data for role

--- modules/cluster/data.tf
+++ modules/cluster/data.tf
@@ -1,1 +1,12 @@
 data "aws_availability_zones" "available" { }
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
