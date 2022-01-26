088bf3e074e85b02d93feb710a81f8d93b9d6dd6
Yagorus <yegor.sidenko@gmail.com>
2022-01-26 18:51:35 +0200

    change network resurses

--- modules/cluster/network.tf
+++ modules/cluster/network.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   count      =  var.az_count
   vpc_id     =  aws_vpc.main.id
-  cidr_block =  cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 1)
+  cidr_block =  cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
   tags = {
@@ -50,8 +50,8 @@ resource "aws_route_table" "public" {
 resource "aws_route_table_association" "routeTableAssociationPublicRoute" {
   depends_on = [aws_route_table.public]
   count = var.az_count
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
-  route_table_id =element(aws_route_table.public.*.id, count.index)
+  subnet_id     = aws_subnet.public[count.index].id
+  route_table_id =aws_route_table.public[count.index].id
 }
 
 /*
