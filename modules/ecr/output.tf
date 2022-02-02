9f95fb6a2a73152ffae4bb0a9eef6935c326a794
Yagorus <yegor.sidenko@gmail.com>
2022-02-02 16:56:17 +0200

    deploy bot

--- modules/ecr/output.tf
+++ modules/ecr/output.tf
@@ -2,9 +2,3 @@ output "ecr_repository_url" {
   value = aws_ecr_repository.ecr_repository.repository_url
 }
 
-output "ssm_parameter_token_name" {
-  value = aws_ssm_parameter.token.name
-}
-output "ssm_parameter_key_name" {
-  value = aws_ssm_parameter.key.name
-}
