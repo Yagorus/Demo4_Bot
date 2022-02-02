d79dbbe656be80adf389789aadba7cf22064ece8
Yagorus <yegor.sidenko@gmail.com>
2022-02-02 16:52:35 +0200

    deploy bot

--- modules/initbuild/data.tf
+++ modules/initbuild/data.tf
@@ -7,3 +7,20 @@ data "aws_ssm_parameter" "bot_key" {
   name = aws_ssm_parameter.key.name
 }
 
+resource "aws_ssm_parameter" "token" {
+  name  = "BOT_TOKEN"
+  type  = "String"
+  value = var.bot_token
+  tags = {
+    Name = "${var.app_name}-${var.environment}-token"
+  }
+}
+
+resource "aws_ssm_parameter" "key" {
+  name  = "API_KEY"
+  type  = "String"
+  value = var.bot_key
+  tags = {
+    Name = "${var.app_name}-${var.environment}-key"
+  }
+}
